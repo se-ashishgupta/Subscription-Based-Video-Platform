8354aab76b11a3bae20a78ae75b8a6f7942e859a
Ashish Gupta <gashish4950@gmail.com>
2023-09-14 22:42:56 +0530

    fixed issues

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ connectDB();
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
-  api_key: process.env.CLOUDINARY_CLIENT_APi,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 

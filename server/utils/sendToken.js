5802f08c1caa3bf7ef0e9e01aa67743b62b15884
Ashish Gupta <gashish4950@gmail.com>
2023-10-03 23:53:09 +0530

    fix logout issue and update login functionality

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -2,8 +2,7 @@ export const sendToken = (res, user, message, statusCode = 200) => {
   const token = user.getJWTToken();
 
   const options = {
-    // expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
-    maxAge: 15 * 24 * 60 * 60 * 1000,
+    expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
     sameSite: "none",
